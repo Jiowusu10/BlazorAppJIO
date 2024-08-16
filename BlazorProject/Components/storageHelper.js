918e7da831ba2a99fa44938faafde0865829abd5
James Owusu Inkum <136287108+Jiowusu10@users.noreply.github.com>
2024-08-16 12:22:50 +0100

    Add files via upload

---
+++ BlazorProject/Components/storageHelper.js
@@ -0,0 +1,8 @@
+﻿window.storageHelper = {
+    setItem: function (key, value) {
+        sessionStorage.setItem(key, value);
+    },
+    getItem: function (key) {
+        return sessionStorage.getItem(key);
+    }
+};
